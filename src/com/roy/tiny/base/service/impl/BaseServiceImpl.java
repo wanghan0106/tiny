95cbb2c2d069bf3c2d049303c7eaf68f9c663302
Roy Wang <wanghan0106@gmail.com>
2014-01-02 10:44:51 +0800

    remove useless import

--- src/com/roy/tiny/base/service/impl/BaseServiceImpl.java
+++ src/com/roy/tiny/base/service/impl/BaseServiceImpl.java
@@ -1,12 +1,8 @@
 package com.roy.tiny.base.service.impl;
 
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,10 +15,8 @@ import com.roy.tiny.base.dao.DaoFactory;
 import com.roy.tiny.base.dao.cond.Cond;
 import com.roy.tiny.base.model.Model;
 import com.roy.tiny.base.service.BaseService;
-import com.roy.tiny.base.service.annotation.Dao;
 import com.roy.tiny.base.web.Pager;
 import com.roy.tiny.base.web.Sorter;
-import com.roy.tiny.topic.service.impl.TopicServiceImpl;
 
 @Transactional(propagation=Propagation.REQUIRED)
 public abstract class BaseServiceImpl<T extends Model> implements BaseService<T> {
